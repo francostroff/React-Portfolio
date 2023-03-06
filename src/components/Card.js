8ed2d244d3d39cc3702885dd813e46839893de7a
francostroff <116038334+francostroff@users.noreply.github.com>
2023-03-06 23:02:42 +0000

    added screenshots

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,25 +6,13 @@ function card() {
     <CardGroup>
       <Card>
         <Card.Img variant="top" src="https://via.placeholder.com/400x200?text=Image+1" />
-      
-        
-       
-        
-      </Card>
+       </Card>
       <Card>
         <Card.Img variant="top" src="https://via.placeholder.com/400x200?text=Image+2" />
-        
-        
-       
-        
-      </Card>
+        </Card>
       <Card>
         <Card.Img variant="top" src="https://via.placeholder.com/400x200?text=Image+3" />
-       
-     
-       
-    
-      </Card>
+       </Card>
     </CardGroup>
   )};
 export default card;
